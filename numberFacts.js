61ab8794d1630302ce8bcd4e668353fad149106e
Brandie Lucano <blucano.dev@gmail.com>
2024-01-01 16:44:29 -0800

    part 2 done (promise race)

--- numberFacts.js
+++ numberFacts.js
@@ -10,4 +10,20 @@ async function showNumberTrivia(){
 
   console.log(data.text)
 }
-showNumberTrivia()
+// showNumberTrivia()
+
+//2.
+/**Makes 4 requests to Numbers API, console logs trivia that returns first. */
+async function showNumberRace(){
+  const n1 = fetch(`${NUMBERS_API_URL}/1?json`);
+  const n2 = fetch(`${NUMBERS_API_URL}/2?json`);
+  const n3 = fetch(`${NUMBERS_API_URL}/3?json`);
+  const n4 = fetch(`${NUMBERS_API_URL}/4?json`);
+
+  const response = await Promise.race([n1, n2, n3, n4]);
+  const data = await response.json()
+
+  console.log(data.text)
+}
+
+showNumberRace()
